58d19735c0ac82f81f5c4a2930bb1f4c64d3b03e
Claudio Moraes <Claudio Moraes>
2016-12-21 20:06:42 -0200

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append(["Claudio Moraes"]);
+
+var awesomeThoughts = "I am Claudio and I am AWESOME!";
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+
+console.log(funThoughts);
+
+$("#main").append([funThoughts])
+
+
+var name = "Claudio Moraes";
+var formattedName =  HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend (formattedRole);
+$("#header").prepend (formattedName);
